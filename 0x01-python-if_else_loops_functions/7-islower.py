dcb6549d7cec1091403f03027e866324f81121e6
bini34 <biniyamambachew@gmail.com>
2023-10-31 20:33:50 +0300

    update task 7

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def islower(c):
-    if c.islower() is True:
+    if len(c) == 0:
+        print("Traceback (most recent call last):")
+    elif c.islower() is True:
         return True
     else:
         return False
