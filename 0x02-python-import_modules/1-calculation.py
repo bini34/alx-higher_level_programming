e582fb102e0721e613f51cc16b883fa364d69d88
bini34 <biniyamambachew@gmail.com>
2023-11-02 12:55:24 +0300

    update task 1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-import calculator_1
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
 
 a = 10
 b = 5
-print("{} + {} = {}".format(a, b, str(calculator_1.add(a, b))))
-print("{} - {} = {}".format(a, b, str(calculator_1.sub(a, b))))
-print("{} * {} = {}".format(a, b, str(calculator_1.mul(a, b))))
-print("{} / {} = {}".format(a, b, str(calculator_1.div(a, b))))
+print("{} + {} = {}".format(a, b, str(add(a, b))))
+print("{} - {} = {}".format(a, b, str(sub(a, b))))
+print("{} * {} = {}".format(a, b, str(mul(a, b))))
+print("{} / {} = {}".format(a, b, str(div(a, b))))
