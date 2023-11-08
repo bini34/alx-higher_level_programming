4ffa2ae61cf973376301034be490da229f6a2ce4
bini34 <biniyamambachew@gmail.com>
2023-11-08 11:01:30 +0300

    add task 0

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    result = []
+    for row in matrix:
+        new_row = []
+        for element in row:
+            squared = element ** 2
+            new_row.append(squared)
+        result.append(new_row)
+    return result
