32a190568fa02e64c1e470a9ae858ffa3769457f
bini34 <biniyamambachew@gmail.com>
2023-12-04 18:54:03 +0300

    add task 3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,14 @@
+#!bin/python3
+
+def is_kind_of_class(obj, a_class):
+    """Check if the object is an instance of, or if it is an instance of
+    a class that inherited from, the specified class.
+
+    Args:
+    - obj: Object to check.
+    - a_class: Class to compare with.
+
+    Returns:
+    - True if obj is an instance of a_class or its subclasses, otherwise False.
+    """
+    return isinstance(obj, a_class)
