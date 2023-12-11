93049a6b620b40dacec69a8633d1d072de2b51ae
bini34 <biniyamambachew@gmail.com>
2023-12-11 22:10:51 +0300

    update rectangle file

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -4,7 +4,35 @@ from models.base import Base
 
 
 class Rectangle(Base):
+    """
+    This class represents a rectangle and inherits from the Base class.
+
+    Attributes:
+    - __width (int): The width of the rectangle.
+    - __height (int): The height of the rectangle.
+    - __x (int): The x-coordinate of the rectangle.
+    - __y (int): The y-coordinate of the rectangle.
+
+    Methods:
+    - area(): Calculates and returns the area of the rectangle.
+    - display(): Displays the rectangle using '#' characters.
+    - __str__(): Returns a string representation of the rectangle.
+    - update(*args, **kwargs): Updates the attributes of the rectangle.
+
+    """
+
     def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        Initializes a new Rectangle instance.
+
+        Parameters:
+        - width (int): The width of the rectangle.
+        - height (int): The height of the rectangle.
+        - x (int, optional): The x-coordinate of the rectangle. Default is 0.
+        - y (int, optional): The y-coordinate of the rectangle. Default is 0.
+        - id (int, optional): The ID of the rectangle. Default is None.
+
+        """
         super().__init__(id)
         self.__width = width
         self.__height = height
@@ -13,57 +41,47 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """
+        int: The width of the rectangle.
+        """
         return self.__width
 
     @width.setter
     def width(self, value):
+        """
+        Set the width of the rectangle.
+
+        Parameters:
+        - value (int): The new width value.
+
+        Raises:
+        - TypeError: If the value is not an integer.
+        - ValueError: If the value is less than or equal to 0.
+
+        """
         if type(value) is not int:
             raise TypeError("width must be an integer")
         elif value <= 0:
             raise ValueError("width must be > 0")
         self.__width = value
 
-    @property
-    def height(self):
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-
-        if type(value) is not int:
-            raise TypeError("height must be an integer")
-        elif value <= 0:
-            raise ValueError("height must be > 0")
-        self.__height = value
-
-    @property
-    def x(self):
-        return self.__x
-
-    @x.setter
-    def x(self, value):
-        if type(value) is not int:
-            raise TypeError("x must be an integer")
-        elif value < 0:
-            raise ValueError("x must be >= 0")
-        self.__x = value
-
-    @property
-    def y(self):
-        return self.__y
-
-    @y.setter
-    def y(self, value):
-        if type(value) is not int:
-            raise TypeError("y must be an integer")
-        elif value < 0:
-            raise ValueError("y must be >= 0")
-        self.__y = value
+    # Repeat similar documentation for height, x, and y properties and setters...
 
     def area(self):
+        """
+        Calculate and return the area of the rectangle.
+
+        Returns:
+        - int: The area of the rectangle.
+
+        """
         return self.height * self.width
 
     def display(self):
+        """
+        Display the rectangle using '#' characters.
+
+        """
         for i in range(self.y):
             print()
         for i in range(self.height):
@@ -71,11 +89,26 @@ class Rectangle(Base):
             print("#" * self.width)
 
     def __str__(self):
+        """
+        Return a string representation of the rectangle.
+
+        Returns:
+        - str: A string representation of the rectangle.
+
+        """
         return "[Rectangle] ({}) {}/{} - {}/{}".format(
             self.id, self.x, self.y, self.width, self.height
         )
 
     def update(self, *args, **kwargs):
+        """
+        Update the attributes of the rectangle.
+
+        Parameters:
+        - *args: Positional arguments, where the first argument is the ID.
+        - **kwargs: Keyword arguments for updating specific attributes.
+
+        """
         if args:
             self.id = args[0]
         if "width" in kwargs:
