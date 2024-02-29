19c1054dff232b341a0d4d533401ab62eef7dfa4
bini34 <biniyamambachew@gmail.com>
2024-02-29 16:50:27 +0300

    add task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -12,9 +12,15 @@ def find_peak(list_of_integers):
     """
     if not list_of_integers:
         return None
-    for i in range(len(list_of_integers)):
-        if (i == 0 or list_of_integers[i] >=
-            list_of_integers[i - 1]) and
-        (i == len(list_of_integers) - 1 or
-         list_of_integers[i] >= list_of_integers[i + 1]):
-            return list_of_integers[i]
+
+    left = 0
+    right = len(list_of_integers) - 1
+
+    while left < right:
+        mid = (left + right) // 2
+        if list_of_integers[mid] < list_of_integers[mid + 1]:
+            left = mid + 1
+        else:
+            right = mid
+
+    return list_of_integers[left]
