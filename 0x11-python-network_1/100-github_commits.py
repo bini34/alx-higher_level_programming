033c026ca9da33c19b35d62ec8e1a51c93a2ff47
bini34 <biniyamambachew@gmail.com>
2024-03-01 22:02:22 +0300

    add task 10

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Script to list 10 commits (from the most recent to oldest)
+of a repository by a given user using the GitHub API
+"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    repo_name = sys.argv[1]
+    owner_name = sys.argv[2]
+    url = f'https://api.github.com/repos/{owner_name}/{repo_name}/commits'
+    response = requests.get(url)
+    if response.status_code == 200:
+        commits = response.json()
+        for commit in commits[:10]:
+            sha = commit['sha']
+            author_name = commit['commit']['author']['name']
+            print(f"{sha}: {author_name}")
+    else:
+        print("Error:", response.status_code)
