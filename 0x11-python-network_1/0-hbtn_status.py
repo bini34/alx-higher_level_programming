1a764ed04c6ebe94ddfde8683959b27d8da586ab
bini34 <biniyamambachew@gmail.com>
2024-03-01 19:50:37 +0300

    update task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-import urllib.request
-"""
-script that fetches https://alx-intranet.hbtn.io/status
+"""script that fetches https://alx-intranet.hbtn.io/status
 """
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    import urllib.request
+
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
-        html = res.read()
+        content = res.read()
         print("Body response:")
-        print("\t- type: {}".format(type(html)))
-        print("\t- content: {}".format(html))
-        print("\t- utf8 content: {}".format(html.decode('utf-8')))
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
